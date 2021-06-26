d5b3bb0f7aafef17e833ebae6349621cefe0bccb
ndlugaszek <n.dlugaszek@gmail.com>
2021-06-27 01:07:40 +0200

    Changed port in api

--- SlideFormApp/SlideFormAppFE/slideformapp/src/components/SlideForm.js
+++ SlideFormApp/SlideFormAppFE/slideformapp/src/components/SlideForm.js
@@ -36,7 +36,7 @@ class SlideForm extends Component {
              Nazwisko: ${lastname} \n
              Adres: ${address} \n
              Płeć: ${usergender}`)
-    fetch('https://localhost:44353/api/form', {
+    fetch('https://localhost:5001/api/form', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json; charset=utf-8',
